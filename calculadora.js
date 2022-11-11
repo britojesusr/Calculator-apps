09e5bc2457cc0a2d9d44639f4e673ef6cea48429
Jesus Brito <jesusrbritol@gmail.com>
2022-11-11 15:19:48 -0400

    darkmode y lightmode agregado con js

--- calculadora.js
+++ calculadora.js
@@ -1,120 +1,86 @@
-function darkmode() {
+const calculatorApp = document.querySelector('#calculatorApp');
+const navApp = document.querySelector('#navApp');
+const title = document.querySelector('#title');
+const arrowToggle = document.querySelector('.arrow-toggle');
+const iconMoon = document.querySelector('.icon-moon');
+const iconSun = document.querySelector('.icon-sun');
 
-  document.getElementById(calculatorApp);
+const iconsWindow = document.querySelectorAll('.icons-window');
+const display = document.querySelector('#display');
+const inputDisplay = document.querySelector('#inputDisplay');
+const divBtnInput = document.querySelector('.btn-inputs');
+const btnInputs = document.querySelectorAll('.btn');
+
+function darkMode() {
+
   calculatorApp.style.backgroundColor='#383838';
-
-  document.getElementById(navApp);
   navApp.style.backgroundColor='#2F2F2F';
-
-  document.getElementById(title);
   title.style.color='#FFFFFF';
+  arrowToggle.setAttribute('src', './assets/arrow-symbolic-dark.svg');
+  iconMoon.classList.add('inactive');
+  iconSun.classList.remove('inactive');
 
-  document.getElementById(display);
+  iconsWindow.forEach( element => element.classList.toggle('inactive'));
+
   display.style.backgroundColor='#3E3E3E';
   display.style.color='#FFF';
   display.style.borderBottom="1px solid #383838";
 
-  document.getElementById(inputDisplay);
   inputDisplay.style.backgroundColor='#404040';
   inputDisplay.style.color='#FFF';
   inputDisplay.style.borderBottom="1px solid #383838";
 
-
-  const iconsLight = document.getElementsByClassName('icon-light');
-
-  for (let i = 0; i < iconsLight.length; i++) {
-    
-    iconsLight[i].style.display = "none";
-    
-  }
-
-  const iconsDark = document.getElementsByClassName('icon-dark');
-  
-  for (let i = 0; i < iconsDark.length; i++) {
-
-    iconsDark[i].style.display = "block";
-
-  }
-
-  const btns = document.getElementsByClassName('btn');
-
-  for (let i = 0; i < btns.length; i++) {
+  for (let i = 0; i < btnInputs.length; i++) {
 
     if(i == 22) {
 
-      btns[22].style.backgroundColor = "#6DB442";
-      btns[22].style.border="1px solid #383838";
+      btnInputs[22].style.backgroundColor = "#6DB442";
+      btnInputs[22].style.border="1px solid #383838";
     }
     
     else {
 
-    btns[i].style.backgroundColor = "#454545";
-    btns[i].style.color="#FFFFFF";
-    btns[i].style.border="1px solid #383838";
-
+    btnInputs[i].style.backgroundColor = "#454545";
+    btnInputs[i].style.color="#FFFFFF";
+    btnInputs[i].style.border="1px solid #383838";
     }
-    
   }
-
 }
 
-function lightmode() {
+function lightMode() {
 
-  document.getElementById(calculatorApp);
   calculatorApp.style.backgroundColor='#F0F0F0';
-
-  document.getElementById(navApp);
   navApp.style.backgroundColor='#D9D9D9';
-
-  document.getElementById(title);
   title.style.color='#6E7375';
+  arrowToggle.setAttribute('src', './assets/arrow-symbolic-light.svg');
+  iconSun.classList.add('inactive');
+  iconMoon.classList.remove('inactive');
+  iconsWindow.forEach( element => element.classList.toggle('inactive'));
 
-  document.getElementById(display);
   display.style.backgroundColor='#F8F8F8';
   display.style.color='#6E7375';
   display.style.borderBottom="1px solid #C0C0C0";
 
-
-  document.getElementById(inputDisplay);
-  inputDisplay.style.backgroundColor='#FFFFFF';
+  inputDisplay.style.backgroundColor='#FFF';
   inputDisplay.style.color='#000';
   inputDisplay.style.borderBottom="1px solid #C0C0C0";
 
-  
-  const iconsDark = document.getElementsByClassName('icon-dark');
-  
-  for (let i = 0; i < iconsDark.length; i++) {
-    
-    iconsDark[i].style.display = "none";
-    
-  }
-  
-  const iconsLight = document.getElementsByClassName('icon-light');
-
-  for (let i = 0; i < iconsLight.length; i++) {
-    
-    iconsLight[i].style.display = "block";
-  
-  }
-
-  const btns = document.getElementsByClassName('btn');
-
-  for (let i = 0; i < btns.length; i++) {
+  for (let i = 0; i < btnInputs.length; i++) {
 
     if(i == 22) {
 
-      btns[22].style.backgroundColor = "#6DB442";
-      btns[i].style.border="1px solid #C0C0C0";        
+      btnInputs[22].style.backgroundColor = "#6DB442";
+      btnInputs[22].style.border="1px solid #C0C0C0";
     }
     
-    else{
-    btns[i].style.backgroundColor = "#FFFFFF";
-    btns[i].style.color="#404040";
-    btns[i].style.border="1px solid #C0C0C0";  
+    else {
+
+    btnInputs[i].style.backgroundColor = "#FFFFFF";
+    btnInputs[i].style.color="#404040";
+    btnInputs[i].style.border="1px solid #C0C0C0";
     }
   }
 }
 
-const btnsInput = document.querySelectorAll('.btn');
-
-
+iconMoon.addEventListener('click', darkMode);
+iconSun.addEventListener('click', lightMode);
